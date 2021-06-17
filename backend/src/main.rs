538d55d7412e58a4c67fa867db79d214e10c9ab9
Stepan Barannikov <mail@tmake.ru>
2021-06-17 14:56:11 +0300

    add test routes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,39 @@
-use std::convert::Infallible;
-use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Request, Response, Server, Method, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
 
-async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello World")))
+// async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+//     Ok(Response::new(Body::from("Hello World")))
+// }
+
+// async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+//     Ok(Response::new("Hello world 2".into()))
+// }
+
+async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(Body::from("This root"))),
+        (&Method::GET, "/echo") => Ok(Response::new(req.into_body())),
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
 }
 
 #[tokio::main]
-async fn main() {
-    // Construct our SocketAddr to listen on...
-    let addr = SocketAddr::from(([0, 0, 0, 0], 7878));
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = ([0, 0, 0, 0], 7878).into();
 
-    // And a MakeService to handle each connection...
-    let make_service = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(handle))
+    let service = make_service_fn(|_| async {
+        Ok::<_, hyper::Error>(service_fn(echo))
     });
 
-    // Then bind and serve...
-    let server = Server::bind(&addr).serve(make_service);
+    let server = Server::bind(&addr).serve(service);
 
-    // And run forever...
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+
+    Ok(())
 }
