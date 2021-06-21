98ed7b2b4c576cfc2d776fd68e15cb267706a83e
Stepan Barannikov <mail@tmake.ru>
2021-06-21 18:26:47 +0300

    add route-recognizer

--- backend/src/handlers/category/mod.rs
+++ backend/src/handlers/category/mod.rs
@@ -1,5 +1,55 @@
-use hyper::{Body, Response};
+use futures::StreamExt;
+use hyper::{Body, Method, Request, Response, StatusCode};
+use route_recognizer::Params;
 
-pub fn index() -> Result<Response<Body>, hyper::Error>{
-    Ok(Response::new("category index".into()))
+use std::{collections::HashMap, convert::TryInto};
+use url::form_urlencoded;
+// use serde::{Serialize, Deserialize};
+
+use crate::handlers::{get_response_by_status_code, ResultResponse};
+
+pub struct CategoryHandle {
+    // list: Vec<Category>
+}
+
+
+// struct Category {
+    // label: String
+// }
+
+// struct Red {
+//     fl: String
+// }
+
+pub fn configure() -> CategoryHandle {
+    CategoryHandle {
+        // list: vec![]
+    }
+}
+
+impl CategoryHandle {
+    pub fn call(&self, req: Request<Body>, params: &Params) -> ResultResponse {
+        match (req.method(), params.find("action")) {
+            (&Method::GET, None) => {
+                println!("Req: {:?}", req);
+                Ok(Response::new("category index".into()))
+            },
+            (&Method::POST, Some("create")) => {
+                // let b = hyper::body::to_bytes(req).await?;
+                // let res = form_urlencoded::parse(b.as_ref())
+                    // .into_owned().collect::<HashMap<String, String>>();
+                let b = hyper::body::to_bytes(&req.body());
+
+                let p = form_urlencoded::parse(b.into())
+                .into_owned()
+                .collect::<HashMap<String, String>>();
+
+                println!("res: {:?}", p);
+
+                Ok(Response::new("category create".into()))
+            },
+            (&_, _) => get_response_by_status_code(StatusCode::NOT_FOUND)
+        }
+    }
+
 }
--- backend/src/handlers/mod.rs
+++ backend/src/handlers/mod.rs
@@ -1,16 +1,24 @@
-use hyper::{Body, Method, Request, Response, StatusCode};
+use hyper::{Body, Request, Response, StatusCode};
+use route_recognizer::Router;
+
+type ResultResponse = Result<Response<Body>, hyper::Error>;
 
 mod category;
 
-pub async fn routes(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/category") => category::index(),
-        (&Method::GET, "/healthcheck") => get_response_by_status_code(StatusCode::OK),
+pub async fn routes(req: Request<Body>) -> ResultResponse {
+    let mut router = Router::new();
+    router.add("/category/*action", category::configure());
+    router.add("/category", category::configure());
+
+    match router.recognize(req.uri().path()) {
+        Ok(handle) => {
+            handle.handler().call(req, handle.params())
+        },
         _ => get_response_by_status_code(StatusCode::NOT_FOUND)
     }
 }
 
-fn get_response_by_status_code(status_code: StatusCode) -> Result<Response<Body>, hyper::Error> {
+fn get_response_by_status_code(status_code: StatusCode) -> ResultResponse {
     let mut response = Response::default();
     *response.status_mut() = status_code;
     Ok(response)
