3d9409a28779fa24ed80be22e7b3193bc9e37683
Stepan Barannikov <mail@tmake.ru>
2021-07-15 18:00:10 +0300

    add db struct and entrypoint in docker

---
+++ backend/db/main.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS category (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    created_at DATETIME NOT NULL,
+    PRIMARY KEY(id)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS task (
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    category_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    created_at DATETIME NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (category_id) REFERENCES category (id)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
